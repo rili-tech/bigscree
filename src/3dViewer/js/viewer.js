9645fb61dcb0f11637b84edb2dff2ad3dc900c54
lixin <377839753@qq.com>
2020-10-22 17:23:10 +0800

    z

--- src/3dViewer/js/viewer.js
+++ src/3dViewer/js/viewer.js
@@ -8,7 +8,7 @@ var selectedObj, originalMaterial;
 var modelCenter;
 
 var urlRoot = "/rlerp";
-var url = window.top.document.referrer
+var url = window.top.document.URL
 var meshList = {};
 console.log(url);
 var projectName = GetQueryString('project')
