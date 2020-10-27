d93204eac0384de65694528cc101d5e34287d864
lixin <377839753@qq.com>
2020-10-27 19:03:46 +0800

    z

--- src/js/turnoverChat.js
+++ src/js/turnoverChat.js
@@ -74,6 +74,7 @@ $(function () {
         }
         else if (time < timeTotal * 2)
         {
+            
             // var iNdx1 = Math.floor((time -timeTotal)/ (intval*2));
             // var data1 = errordata[iNdx1];
             // time % intval ? data1.color="#FFA500" : data1.color="black";
